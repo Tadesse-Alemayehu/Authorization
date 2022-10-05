de1dcbaadd9c73b5ef1f5195f2b68c2de3342ad1
Tadesse-Alemayehu <tadalem26@gmail.com>
2022-10-05 16:23:36 +0300

    Create: user authentication

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,5 +3,15 @@ class UsersController < ApplicationController
     @new_user=User.new
   end
   def create
+    @user=User.create(user_params)
+    if @user
+      flash[:success]=" user created successfully"
+      redirect_to users_new_path
+    end
   end
+
+  private
+   def user_params
+    params.require(:user).permit(:email, :password)
+   end
 end
